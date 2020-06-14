150638daed31edddb95807136309d12713cb12a7
fred62879 <zhuotingxie-fred@outlook.com>
2020-06-14 01:42:02 -0700

    db class

---
+++ src/db/MovieAward.java
@@ -0,0 +1,24 @@
+package db;
+
+public class MovieAward {
+
+    public MovieAward() {}
+
+    public void insert() {}
+
+    public void delete() {}
+
+    public void update() {}
+
+    public void select() {}
+
+    public void project() {}
+
+    public void join() {}
+
+    public void aggregate() {}
+
+    public void nestedAggregate() {}
+
+    public void divide() {}
+}
